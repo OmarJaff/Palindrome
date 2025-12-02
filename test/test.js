d0901d342636199cbc9bd292fc071b91cb10f7b5
OmarJaff <me@omarjaff.com>
2025-12-02 15:46:46 +0300

    Write test scinario for palindrome method

--- test/test.js
+++ test/test.js
@@ -4,5 +4,22 @@ let Phrase = require("../index.js");
 describe("Phrase", function() {
     describe("#palindrome", function() {
         
+        it("should return false for a non palindrome", function(){
+            let nonPalindrome = new Phrase('apple');
+            assert(!nonPalindrome.palindrome());
+        });
+
+        it("should return true for a plain palindrome", function() {
+            let plainPalindrome = new Phrase('racecar');
+            assert(plainPalindrome.palindrome());
+        });
+
+        it("should return true for mixed-case plaindrome", function() {
+            let mixedCasePlaindrome = new Phrase('RacecaR');
+            assert(mixedCasePlaindrome.palindrome());
+        })
+
+        it("should return true for a plaindrome with punctuation");
+
     })
 })
